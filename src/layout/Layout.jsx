ae1c0f657be8b7e35b0bd301a58c7c26537e51e0
Slinkter <luis.j.cueva@gmail.com>
2024-07-17 18:28:42 -0500

    update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,6 @@
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import { Box, Flex } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
     return (
