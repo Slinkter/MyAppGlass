42cba211555431bf5dae93645dfe0a275543971c
Slinkter <luis.j.cueva@gmail.com>
2024-07-17 18:28:42 -0500

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,308 @@
 import {
     Box,
     Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Icon,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+} from "@chakra-ui/react";
+import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+} from "@chakra-ui/icons";
+
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Box>
+            <Flex
+                minH={"60px"}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle={"solid"}
+                borderColor={useColorModeValue("gray.200", "gray.900")}
+                align={"center"}
+            >
+                <Flex
+                    flex={{ base: 1, md: "auto" }}
+                    ml={{ base: -2 }}
+                    display={{ base: "flex", md: "none" }}
+                >
+                    <IconButton
+                        onClick={onToggle}
+                        icon={
+                            isOpen ? (
+                                <CloseIcon w={3} h={3} />
+                            ) : (
+                                <HamburgerIcon w={5} h={5} />
+                            )
+                        }
+                        aria-label={"Toggle Navigation"}
+                        variant={"ghost"}
+                    />
+                </Flex>
+                <Flex
+                    flex={{ base: 1 }}
+                    justify={{ base: "center", md: "center" }}
+                >
+                    <Flex display={{ base: "none", md: "flex" }} ml={10}>
+                        <DesktopNav />
+                    </Flex>
+                </Flex>
+            </Flex>
+
+            <Collapse in={isOpen} animateOpacity>
+                <MobileNav />
+            </Collapse>
+        </Box>
+    );
+}
+
+const DesktopNav = () => {
+    const linkColor = useColorModeValue("gray.600", "gray.200");
+    const linkHoverColor = useColorModeValue("gray.800", "white");
+    const popoverContentBgColor = useColorModeValue("white", "gray.800");
+
+    return (
+        <Stack direction={"row"} spacing={4}>
+            {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                    <Popover trigger={"hover"} placement={"bottom-start"}>
+                        <PopoverTrigger>
+                            <Box
+                                as="a"
+                                p={2}
+                                href={navItem.href ?? "#"}
+                                fontSize={"sm"}
+                                fontWeight={500}
+                                color={linkColor}
+                                _hover={{
+                                    textDecoration: "none",
+                                    color: linkHoverColor,
+                                }}
+                            >
+                                {navItem.label}
+                            </Box>
+                        </PopoverTrigger>
+
+                        {navItem.children && (
+                            <PopoverContent
+                                border={0}
+                                boxShadow={"xl"}
+                                bg={popoverContentBgColor}
+                                p={4}
+                                rounded={"xl"}
+                                minW={"sm"}
+                            >
+                                <Stack>
+                                    {navItem.children.map((child) => (
+                                        <DesktopSubNav
+                                            key={child.label}
+                                            {...child}
+                                        />
+                                    ))}
+                                </Stack>
+                            </PopoverContent>
+                        )}
+                    </Popover>
+                </Box>
+            ))}
+        </Stack>
+    );
+};
+
+const DesktopSubNav = ({ label, href, subLabel }) => {
+    return (
+        <Box
+            as="a"
+            href={href}
+            role={"group"}
+            display={"block"}
+            p={2}
+            rounded={"md"}
+            _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+        >
+            <Stack direction={"row"} align={"center"}>
+                <Box>
+                    <Text
+                        transition={"all .3s ease"}
+                        _groupHover={{ color: "pink.400" }}
+                        fontWeight={500}
+                    >
+                        {label}
+                    </Text>
+                    <Text fontSize={"sm"}>{subLabel}</Text>
+                </Box>
+                <Flex
+                    transition={"all .3s ease"}
+                    transform={"translateX(-10px)"}
+                    opacity={0}
+                    _groupHover={{
+                        opacity: "100%",
+                        transform: "translateX(0)",
+                    }}
+                    justify={"flex-end"}
+                    align={"center"}
+                    flex={1}
+                >
+                    <Icon
+                        color={"pink.400"}
+                        w={5}
+                        h={5}
+                        as={ChevronRightIcon}
+                    />
+                </Flex>
+            </Stack>
+        </Box>
+    );
+};
+
+const MobileNav = () => {
+    return (
+        <Stack
+            bg={useColorModeValue("white", "gray.800")}
+            p={4}
+            display={{ md: "none" }}
+        >
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    );
+};
+
+const MobileNavItem = ({ label, children, href }) => {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Stack spacing={4} onClick={children && onToggle}>
+            <Box
+                py={2}
+                as="a"
+                href={href ?? "#"}
+                justifyContent="space-between"
+                alignItems="center"
+                _hover={{
+                    textDecoration: "none",
+                }}
+            >
+                <Text
+                    fontWeight={600}
+                    color={useColorModeValue("gray.600", "gray.200")}
+                >
+                    {label}
+                </Text>
+                {children && (
+                    <Icon
+                        as={ChevronDownIcon}
+                        transition={"all .25s ease-in-out"}
+                        transform={isOpen ? "rotate(180deg)" : ""}
+                        w={6}
+                        h={6}
+                    />
+                )}
+            </Box>
+
+            <Collapse
+                in={isOpen}
+                animateOpacity
+                style={{ marginTop: "0!important" }}
+            >
+                <Stack
+                    mt={2}
+                    pl={4}
+                    borderLeft={1}
+                    borderStyle={"solid"}
+                    borderColor={useColorModeValue("gray.200", "gray.700")}
+                    align={"start"}
+                >
+                    {children &&
+                        children.map((child) => (
+                            <Box
+                                as="a"
+                                key={child.label}
+                                py={2}
+                                href={child.href}
+                            >
+                                {child.label}
+                            </Box>
+                        ))}
+                </Stack>
+            </Collapse>
+        </Stack>
+    );
+};
+
+const NAV_ITEMS = [
+    {
+        label: "Home",
+        children: [
+            {
+                label: "Explore Design Work",
+                subLabel: "Trending Design to inspire you",
+                href: "#",
+            },
+            {
+                label: "New & Noteworthy",
+                subLabel: "Up-and-coming Designers",
+                href: "#",
+            },
+        ],
+    },
+    {
+        label: "Servicios",
+        children: [
+            {
+                label: "Ventanas",
+                subLabel: "Find your dream design job",
+                href: "#",
+            },
+            {
+                label: "Mamparas",
+                subLabel: "An exclusive list for contract work",
+                href: "#",
+            },
+            {
+                label: "Puerta de ducha",
+                subLabel: "An exclusive list for contract work",
+                href: "#",
+            },
+            {
+                label: "as servicios",
+                subLabel: "An exclusive list for contract work",
+                href: "#",
+            },
+        ],
+    },
+    {
+        label: "Productos",
+        href: "#",
+    },
+    {
+        label: "Proyectos",
+        href: "#",
+    },
+    {
+        label: "Nosotros",
+        href: "#",
+    },
+];
+
+/* import {
+    Box,
+    Flex,
     Link,
     IconButton,
     useDisclosure,
@@ -15,29 +317,28 @@ const Navbar = () => {
     const { isOpen, onToggle } = useDisclosure();
 
     return (
-        <Flex p={2} pos={"relative"} w={"full"} flexDir={"row-reverse"}>
+        <Box p={2} pos={"relative"} w={"full"} flexDir={"row-reverse"} h={0}>
             <Flex
-                mx={"auto"}
+                mx="auto"
                 maxW="1200px"
-                justifyContent={{ base: "center", md: "center" }}
+                justifyContent="center"
+                alignItems="center"
                 wrap="wrap"
-                alignSelf={"center"}
             >
                 <Show above="md">
                     <Flex
-                        direction={{ base: "column", md: "row" }}
-                        display={{ base: "none", md: "flex" }}
-                        mt={{ base: 4, md: 0 }}
-                        justifyContent="space-between"
+                        direction="row"
+                        display="flex"
+                        mt={0}
+                        justifyContent="center"
                         alignItems="center"
-                        transition={"all 0.3 ease-in"}
+                        transition="all 0.3s ease-in"
                     >
                         <Stack
-                            direction={{ base: "column", md: "row" }}
+                            direction="row"
                             spacing={4}
                             align="center"
-                            justifyContent="space-between"
-                            alignItems="center"
+                            justifyContent="center"
                         >
                             <Link as={RouterLink} to="/" p={2}>
                                 Inicio
@@ -58,63 +359,63 @@ const Navbar = () => {
                     </Flex>
                 </Show>
 
-                <Hide above="md" alignSelf="left">
-                    <IconButton
+                <Hide above="md">
+                    <Flex
+                        flexDir={"column"}
                         position={"relative"}
-                        onClick={onToggle}
-                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                        variant="outline"
-                        aria-label="Toggle Navigation"
-                    />
-
-                    <Collapse
-                        in={isOpen}
-                        animateOpacity
-                        transition={{
-                            exit: { delay: 1.2 },
-                            enter: { duration: 0.5 },
-                        }}
+                        w={"95vw"}
+                        border={"1px solid red"}
+                        height={"auto"}
                     >
-                        <Flex
-                            direction={{ base: "column", md: "row" }}
-                            display={{
-                                base: isOpen ? "flex" : "none",
-                                md: "flex",
-                            }}
-                            mt={{ base: 4, md: 0 }}
-                            justifyContent="space-between"
-                            alignItems="center"
-                            transition={"all 0.3 ease-in"}
-                        >
-                            <Stack
-                                direction={{ base: "column", md: "row" }}
-                                spacing={4}
-                                align="center"
-                                justifyContent="space-between"
+                        <IconButton
+                            onClick={onToggle}
+                            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                            variant="outline"
+                            aria-label="Toggle Navigation"
+                            alignSelf="flex-start"
+                            position={"absolute"}
+                            right={0}
+                        />
+
+                        <Collapse in={isOpen} animateOpacity>
+                            <Flex
+                                direction="column"
+                                display={isOpen ? "flex" : "none"}
+                                mt={4}
+                                justifyContent="center"
                                 alignItems="center"
+                                transition="all 0.3s ease-in"
                             >
-                                <Link as={RouterLink} to="/" p={2}>
-                                    Inicio
-                                </Link>
-                                <Link as={RouterLink} to="/servicios" p={2}>
-                                    Servicios
-                                </Link>
-                                <Link as={RouterLink} to="/producto" p={2}>
-                                    Productos
-                                </Link>
-                                <Link as={RouterLink} to="/proyectos" p={2}>
-                                    Proyectos
-                                </Link>
-                                <Link as={RouterLink} to="/nosotros" p={2}>
-                                    Nosotros
-                                </Link>
-                            </Stack>
-                        </Flex>
-                    </Collapse>
+                                <Stack
+                                    direction="column"
+                                    spacing={4}
+                                    align="center"
+                                    justifyContent="center"
+                                >
+                                    <Link as={RouterLink} to="/" p={2}>
+                                        Inicio
+                                    </Link>
+                                    <Link as={RouterLink} to="/servicios" p={2}>
+                                        Servicios
+                                    </Link>
+                                    <Link as={RouterLink} to="/producto" p={2}>
+                                        Productos
+                                    </Link>
+                                    <Link as={RouterLink} to="/proyectos" p={2}>
+                                        Proyectos
+                                    </Link>
+                                    <Link as={RouterLink} to="/nosotros" p={2}>
+                                        Nosotros
+                                    </Link>
+                                </Stack>
+                            </Flex>
+                        </Collapse>
+                    </Flex>
                 </Hide>
             </Flex>
-        </Flex>
+        </Box>
     );
 };
 
 export default Navbar;
+ */
