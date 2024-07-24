69d6f3ddc63b95479ceb74abd85cd408b9d09ff3
Slinkter <luis.j.cueva@gmail.com>
2024-07-24 11:07:31 -0500

    update

--- src/components/HomePage/Feature.jsx
+++ src/components/HomePage/Feature.jsx
@@ -28,43 +28,43 @@ const features = [
             "Recibe una estimación rápida y precisa a través de WhatsApp. Contáctanos al 996-537-435.",
     },
     {
-        heading: "Agenda una Visita Técnica",
+        heading: " Visita Técnica",
         icon: IoIosCalendar,
         description:
             "Coordina una visita técnica sin costo adicional. Nuestro equipo evaluará tus necesidades en persona.",
     },
     {
-        heading: "Materiales y Accesorios de Calidad",
+        heading: "Materiales  ",
         icon: IoIosKeypad,
         description:
             "Ofrecemos una garantía de 3 meses en todos nuestros productos instalados.",
     },
     {
-        heading: "Trabajadores Capacitados",
+        heading: "Técnicos Capacitados",
         icon: IoMdConstruct,
         description:
             "Nuestro equipo de operarios está altamente capacitado y cuenta con amplia experiencia en instalaciones.",
     },
     {
-        heading: "Garantía del Producto",
+        heading: "Garantías",
         icon: IoMdSwap,
         description:
             "Disfruta de una garantía de 6 meses en todos nuestros productos instalados.",
     },
     {
-        heading: "Depósitos y Pagos Seguros",
+        heading: "Método de pago",
         icon: HiOutlineBanknotes,
         description:
             "Disponemos de una cuenta corriente empresarial y aceptamos tarjetas de crédito y débito.",
     },
     {
-        heading: "Boleta y/o Factura Electrónica",
+        heading: "Recibos ",
         icon: IoIosPaper,
         description:
             "Emitimos boletas y facturas electrónicas inmediatamente después de realizar el pago.",
     },
     {
-        heading: "Ofertas y Promociones Mensuales",
+        heading: " Promociones ",
         icon: IoMdPricetags,
         description:
             "Descubre nuestras ofertas y promociones actualizadas cada mes en una variedad de productos y servicios.",
@@ -88,7 +88,7 @@ const Feature = () => {
                     minHeight={"80vh"}
                 >
                     <SimpleGrid
-                        columns={isMobile ? 2 : 4}
+                        columns={isMobile ? 1 : 4}
                         spacingX={isMobile ? "20px" : "30px"}
                         spacingY={isMobile ? "20px" : "30px"}
                     >
--- src/components/HomePage/FeatureCard.jsx
+++ src/components/HomePage/FeatureCard.jsx
@@ -1,56 +1,52 @@
 import {
     Box,
-    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
     Flex,
     Heading,
-    Stack,
     Text,
     useColorModeValue,
     useMediaQuery,
+    Button,
 } from "@chakra-ui/react";
 import React from "react";
 
 const FeatureCard = ({ heading, description, icon, href }) => {
-    const [isMobile] = useMediaQuery("(max-width: 768px)"); // Adjust breakpoint as needed
+    const [isMobile] = useMediaQuery("(max-width: 768px)"); // Ajusta el punto de quiebre según sea necesario
     return (
-        <Box
+        <Card
             w={"full"}
             maxW={{ base: "full", md: "375px" }}
-            borderWidth="1px"
             overflow="hidden"
             p={isMobile ? 3 : 6}
             boxShadow={{ base: "base", md: "lg" }}
             pos={"relative"}
             rounded={"lg"}
         >
-            <Stack mb={6} pt={2} align={"center"} spacing={2}>
-                <Stack pt={4} align={"center"}>
-                    <Flex
-                        w={20}
-                        h={20}
-                        align={"center"}
-                        justify={"center"}
-                        rounded={"full"}
-                        bg={useColorModeValue("gray.100", "gray.700")}
-                    >
-                        {icon}
-                    </Flex>
-                    <Box mt={2} align={"center"}>
-                        <Heading size="md" mb={3}>
-                            {heading}
-                        </Heading>
-                        <Text
-                            mt={1}
-                            fontSize={"sm"}
-                            color={"gray.500"}
-                            align={"center"}
-                        >
-                            {description}
-                        </Text>
-                    </Box>
-                </Stack>
-            </Stack>
-        </Box>
+            <CardHeader>
+                <Flex
+                    w={20}
+                    h={20}
+                    align={"center"}
+                    justify={"center"}
+                    rounded={"full"}
+                    bg={useColorModeValue("gray.100", "gray.700")}
+                    mx="auto"
+                >
+                    {icon}
+                </Flex>
+            </CardHeader>
+            <CardBody textAlign="center">
+                <Heading size="md" mb={3}>
+                    {heading}
+                </Heading>
+                <Text mt={1} fontSize={"sm"} color={"gray.500"}>
+                    {description}
+                </Text>
+            </CardBody>
+        </Card>
     );
 };
 
