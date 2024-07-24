f731c4a0bfdfaaf497a1d4af8795d096cad1df1f
Slinkter <luis.j.cueva@gmail.com>
2024-07-24 17:28:02 -0500

    update

---
+++ src/components/ServicePage/db_service.js
@@ -0,0 +1,95 @@
+import ventana from "../../assets/webService/init01.jpg";
+import Mampara from "../../assets/webService/init02a.jpg";
+import puertaducha from "../../assets/webService/init03.jpg";
+import techopoli from "../../assets/webService/init04.jpg";
+import parapeto from "../../assets/webService/init05a.jpg";
+import baranda from "../../assets/webService/init06.jpg";
+import balcon from "../../assets/webService/init07a.jpg";
+import muro from "../../assets/webService/init08.jpg";
+import lamseg from "../../assets/webService/init09.jpg";
+import areanado from "../../assets/webService/init10.jpg";
+import puertavidrio from "../../assets/webService/init11.jpg";
+import puertaserie from "../../assets/webService/init12.jpg";
+import celocias from "../../assets/webService/init13.jpg";
+import frenos from "../../assets/webService/init14.jpg";
+import fachada from "../../assets/webService/init15.jpg";
+import mosquetero from "../../assets/webService/init16.jpg";
+
+const listService = [
+    {
+        id: 1,
+        image: ventana,
+        name: "Ventanas",
+        link: "https://bit.ly/3HuT0Eq",
+        plink: "/service/ventana",
+    },
+    {
+        id: 2,
+        image: Mampara,
+        name: "Mamparas",
+        link: "https://www.facebook.com/media/set/?set=a.414060650462803&type=3/?from=GoogleBT",
+        plink: "/service/mampara",
+    },
+    {
+        id: 3,
+        image: puertaducha,
+        name: "Puertas de Ducha ",
+        link: "https://www.facebook.com/media/set/?set=a.414060420462826&type=3?from=GoogleBT",
+        plink: "/service/ducha",
+    },
+
+    {
+        id: 4,
+        image: parapeto,
+        name: "Parapeto de vidrio",
+        link: "https://www.facebook.com/media/set/?set=a.414060023796199&type=3?from=GoogleBT",
+        plink: "/service/parapeto",
+    },
+    {
+        id: 5,
+        image: baranda,
+        name: "Barandas de Acero",
+        link: "https://www.facebook.com/gyacompany?from=GoogleBT",
+        plink: "/service/baranda",
+    },
+    {
+        id: 6,
+        image: balcon,
+        name: "Balcones",
+        link: "https://www.facebook.com/media/set/?set=a.414059690462899&type=3?from=GoogleBT",
+        plink: "/service/balcones",
+    },
+
+    {
+        id: 7,
+        image: techopoli,
+        name: "Techo de Policarbonato",
+        link: "https://www.facebook.com/media/set/?set=a.414060237129511&type=3?from=GoogleBT",
+        plink: "/service/techo-poli",
+    },
+
+    {
+        id: 8,
+        image: puertavidrio,
+        name: "Puerta de Vidrio",
+        link: "https://www.facebook.com/gyacompany/?from=GoogleBT",
+        plink: "/service/puertav",
+    },
+    {
+        id: 9,
+        image: puertaserie,
+        name: "Puerta en Serie ",
+        link: "https://www.facebook.com/gyacompany/?from=GoogleBT",
+        plink: "/service/puertas",
+    },
+
+    {
+        id: 10,
+        image: celocias,
+        name: "Celosías",
+        link: "https://www.facebook.com/gyacompany/?from=GoogleBT",
+        plink: "/service/celosias",
+    },
+];
+
+export default listService;
