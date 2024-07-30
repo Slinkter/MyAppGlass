df622c9a5f9dc91db9446ae56289b5e067cf94b0
Slinkter <luis.j.cueva@gmail.com>
2024-07-30 18:22:42 -0500

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,56 +19,6 @@ import {
     ChevronRightIcon,
 } from "@chakra-ui/icons";
 
-export default function WithSubnavigation() {
-    const { isOpen, onToggle } = useDisclosure();
-
-    return (
-        <Box>
-            <Flex
-                minH={"50px"}
-                py={{ base: 2 }}
-                px={{ base: 4 }}
-                borderBottom={2}
-                borderStyle={"solid"}
-                borderColor={useColorModeValue("gray.100", "gray.900")}
-                align={"center"}
-            >
-                <Flex
-                    flex={{ base: 1, md: "auto" }}
-                    ml={{ base: -2 }}
-                    display={{ base: "flex", md: "none" }}
-                >
-                    <IconButton
-                        onClick={onToggle}
-                        icon={
-                            isOpen ? (
-                                <CloseIcon w={3} h={3} />
-                            ) : (
-                                <HamburgerIcon w={5} h={5} />
-                            )
-                        }
-                        aria-label={"Toggle Navigation"}
-                        variant={"ghost"}
-                    />
-                </Flex>
-                <Flex
-                    flex={{ base: 1 }}
-                    justify={{ base: "center", md: "center" }}
-                >
-                    <Flex display={{ base: "none", md: "flex" }} ml={10}>
-                        <DesktopNav />
-                    </Flex>
-                </Flex>
-            </Flex>
-
-            <Collapse in={isOpen} animateOpacity>
-                <MobileNav />
-            </Collapse>
-        </Box>
-    );
-}
-
-/*  */
 const NAV_ITEMS = [
     {
         label: "Home",
@@ -130,6 +80,61 @@ const NAV_ITEMS = [
         href: "#",
     },
 ];
+
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Box
+            position={{ base: "", md: "" }}
+            zIndex={{ base: "", md: 999 }}
+            w={"100vw"}
+        >
+            <Flex
+                minH={"50px"}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={2}
+                borderStyle={"solid"}
+                borderColor={useColorModeValue("gray.100", "gray.900")}
+                align={"center"}
+            >
+                <Flex
+                    flex={{ base: 1, md: "auto" }}
+                    ml={{ base: -2 }}
+                    display={{ base: "flex", md: "none" }}
+                >
+                    <IconButton
+                        onClick={onToggle}
+                        icon={
+                            isOpen ? (
+                                <CloseIcon w={3} h={3} />
+                            ) : (
+                                <HamburgerIcon w={5} h={5} />
+                            )
+                        }
+                        aria-label={"Toggle Navigation"}
+                        variant={"ghost"}
+                    />
+                </Flex>
+                <Flex
+                    flex={{ base: 1 }}
+                    justify={{ base: "center", md: "center" }}
+                >
+                    <Flex display={{ base: "none", md: "flex" }} ml={10}>
+                        <DesktopNav />
+                    </Flex>
+                </Flex>
+            </Flex>
+
+            <Collapse in={isOpen} animateOpacity>
+                <MobileNav />
+            </Collapse>
+        </Box>
+    );
+}
+
+/*  */
 
 const DesktopNav = () => {
     const linkColor = useColorModeValue("gray.600", "gray.200");
