b41ca6ca73b30b6e707d62a56241cf0f6d3e2f8b
Slinkter <luis.j.cueva@gmail.com>
2024-08-03 10:17:21 -0500

    sabado : init

--- src/components/ServicePage/8-PVidrio/Pvidrio.jsx
+++ src/components/ServicePage/8-PVidrio/Pvidrio.jsx
@@ -1,7 +1,205 @@
-import React from "react";
+import {
+    Box,
+    Icon,
+    Text,
+    Stack,
+    Grid,
+    GridItem,
+    Image,
+    useColorModeValue,
+    useBreakpointValue,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalCloseButton,
+    Skeleton,
+    useMediaQuery,
+    Container,
+    Heading,
+    Flex,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { listPuertav } from "../../../assets/webService/s/11.PuertaV/db_puertav";
 
 const Pvidrio = () => {
-    return <div>Pvidrio</div>;
+    window.document.title = "Pvidrio";
+    const [loading, setLoading] = useState(true);
+
+    setTimeout(() => {
+        setLoading(false);
+    }, 1000);
+    return (
+        <Box display={"flex"} flexDir={{ base: "column", md: "row" }}>
+            <Box
+                ml="2rem"
+                mt="2rem"
+                mr={{ base: "2rem", md: "2rem" }}
+                p={4}
+                bg={useColorModeValue("white", "gray.800")}
+                h={{ base: "auto", md: "80vh" }}
+                w={{ base: "full-20rem", md: "15rem" }}
+                rounded="lg"
+                boxShadow="md"
+                border={"1px solid "}
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+                _hover={{
+                    borderColor: "gray.300",
+                    boxShadow: "lg",
+                }}
+            >
+                <Box mb={2} p={4}>
+                    <Heading as="h2" fontWeight="600" color="red.500" mt={4}>
+                        Puerta de Vidrio
+                    </Heading>
+
+                    <Stack spacing={1}>
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Vidrio templado de 8mm"
+                            loading={loading}
+                        />
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Chapa bola "
+                            loading={loading}
+                        />
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Accesorios "
+                            loading={loading}
+                        />
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Arenado con diseño"
+                            loading={loading}
+                        />
+                    </Stack>
+                </Box>
+            </Box>
+
+            <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                mt="2rem"
+                mr="2rem"
+                mb="2rem"
+                ml={{ base: "2rem", md: "0rem" }}
+                p={4}
+                bg={useColorModeValue("white", "gray.800")}
+                h={{ base: "auto", md: "80vh" }}
+                rounded="lg"
+                boxShadow="md"
+                border={"1px solid "}
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+                _hover={{
+                    borderColor: "gray.300",
+                    boxShadow: "lg",
+                }}
+            >
+                <Gallery images={listPuertav.puertav} loading={loading} />
+            </Box>
+        </Box>
+    );
 };
 
 export default Pvidrio;
+
+const Gallery = ({ images, loading }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isMobile] = useMediaQuery();
+    const resposiveColmns = useBreakpointValue({ base: 1, md: 4 });
+    const onClose = () => setIsOpen(false);
+
+    console.log("images", images);
+    return (
+        <>
+            <Grid templateColumns={`repeat(${resposiveColmns} ,1fr)`} gap={4}>
+                {images.map((item) => (
+                    <GridItem key={item.id}>
+                        {loading ? (
+                            <Skeleton
+                                w={{ base: "full", md: "18vw" }}
+                                minH={"24vh"}
+                                h={"24vh"}
+                                rounded={"lg"}
+                            />
+                        ) : (
+                            <Image
+                                w={{ base: "full", md: "18vw" }}
+                                minH={"24vh"}
+                                h={"24vh"}
+                                rounded={"lg"}
+                                src={item.image}
+                                objectFit={"cover"}
+                                mb={{ base: 4, md: 0 }}
+                                shadow={"base"}
+                                cursor={"pointer"}
+                                onClick={() => {
+                                    setSelectedImage(item.image);
+                                    setIsOpen(true);
+                                }}
+                            />
+                        )}
+                    </GridItem>
+                ))}
+            </Grid>
+            {!isMobile && (
+                <Modal
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    isCentered
+                    motionPreset="slideInBottom"
+                >
+                    <ModalOverlay backdropFilter={"blur(10px)"} />
+                    <ModalContent>
+                        <ModalCloseButton />
+                        <Image
+                            src={selectedImage}
+                            alt={selectedImage}
+                            objectFit={"cover"}
+                            align={"center"}
+                            borderRadius={"base"}
+                            shadow={"lg"}
+                        />
+                    </ModalContent>
+                </Modal>
+            )}
+        </>
+    );
+};
+
+const SidebarItem = ({ icon, title, onClick, loading }) => {
+    return (
+        <Stack
+            direction={"row"}
+            align={"center"}
+            justifyContent={"space-between"}
+            onClick={onClick}
+            cursor={"pointer"}
+            p={2}
+            rounded={"md"}
+            _hover={{
+                bg: useColorModeValue("gray.50", "gray.900"),
+                color: "red.500",
+            }}
+            transition={"all .1s ease"}
+        >
+            <Stack direction={"row"} align={"center"} spacing={4}>
+                {loading ? (
+                    <>
+                        <Skeleton height={"20px"} width={"20px"} />
+                        <Skeleton height={"20px"} width={"60px"} />
+                    </>
+                ) : (
+                    <>
+                        <Icon as={icon} w={5} h={5} />
+                        <Text fontWeight={500}>{title}</Text>
+                    </>
+                )}
+            </Stack>
+        </Stack>
+    );
+};
--- src/components/ServicePage/9-PSerie/Pserie.jsx
+++ src/components/ServicePage/9-PSerie/Pserie.jsx
@@ -1,7 +1,201 @@
-import React from "react";
+import {
+    Box,
+    Icon,
+    Text,
+    Stack,
+    Grid,
+    GridItem,
+    Image,
+    useColorModeValue,
+    useBreakpointValue,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalCloseButton,
+    Skeleton,
+    useMediaQuery,
+    Container,
+    Heading,
+    Flex,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { listPuertas } from "../../../assets/webService/s/12.PuertaS/db_puertas";
 
 const Pserie = () => {
-    return <div>Pserie</div>;
+    window.document.title = "Puerta de serie";
+    const [loading, setLoading] = useState(true);
+
+    setTimeout(() => {
+        setLoading(false);
+    }, 1000);
+
+    return (
+        <Box display={"flex"} flexDir={{ base: "column", md: "row" }}>
+            <Box
+                ml="2rem"
+                mt="2rem"
+                mr={{ base: "2rem", md: "2rem" }}
+                p={4}
+                bg={useColorModeValue("white", "gray.800")}
+                h={{ base: "auto", md: "80vh" }}
+                w={{ base: "full-20rem", md: "15rem" }}
+                rounded="lg"
+                boxShadow="md"
+                border={"1px solid "}
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+                _hover={{
+                    borderColor: "gray.300",
+                    boxShadow: "lg",
+                }}
+            >
+                <Box mb={2} p={4}>
+                    <Heading as="h2" fontWeight="600" color="red.500" mt={4}>
+                        Puerta de serie
+                    </Heading>
+
+                    <Stack spacing={1}>
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Estructura de Aluminio "
+                            loading={loading}
+                        />
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Chapa bola de Aluminio"
+                            loading={loading}
+                        />
+                        <SidebarItem
+                            icon={PaperAirplaneIcon}
+                            title="Plancha Arenado"
+                            loading={loading}
+                        />
+                    </Stack>
+                </Box>
+            </Box>
+
+            <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                mt="2rem"
+                mr="2rem"
+                mb="2rem"
+                ml={{ base: "2rem", md: "0rem" }}
+                p={4}
+                bg={useColorModeValue("white", "gray.800")}
+                h={{ base: "auto", md: "80vh" }}
+                rounded="lg"
+                boxShadow="md"
+                border={"1px solid "}
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+                _hover={{
+                    borderColor: "gray.300",
+                    boxShadow: "lg",
+                }}
+            >
+                <Gallery images={listPuertas.puertas} loading={loading} />
+            </Box>
+        </Box>
+    );
 };
 
 export default Pserie;
+
+const Gallery = ({ images, loading }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isMobile] = useMediaQuery();
+    const resposiveColmns = useBreakpointValue({ base: 1, md: 4 });
+    const onClose = () => setIsOpen(false);
+
+    console.log("images", images);
+    return (
+        <>
+            <Grid templateColumns={`repeat(${resposiveColmns} ,1fr)`} gap={4}>
+                {images.map((item) => (
+                    <GridItem key={item.id}>
+                        {loading ? (
+                            <Skeleton
+                                w={{ base: "full", md: "18vw" }}
+                                minH={"24vh"}
+                                h={"24vh"}
+                                rounded={"lg"}
+                            />
+                        ) : (
+                            <Image
+                                w={{ base: "full", md: "18vw" }}
+                                minH={"24vh"}
+                                h={"24vh"}
+                                rounded={"lg"}
+                                src={item.image}
+                                objectFit={"cover"}
+                                mb={{ base: 4, md: 0 }}
+                                shadow={"base"}
+                                cursor={"pointer"}
+                                onClick={() => {
+                                    setSelectedImage(item.image);
+                                    setIsOpen(true);
+                                }}
+                            />
+                        )}
+                    </GridItem>
+                ))}
+            </Grid>
+            {!isMobile && (
+                <Modal
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    isCentered
+                    motionPreset="slideInBottom"
+                >
+                    <ModalOverlay backdropFilter={"blur(10px)"} />
+                    <ModalContent>
+                        <ModalCloseButton />
+                        <Image
+                            src={selectedImage}
+                            alt={selectedImage}
+                            objectFit={"cover"}
+                            align={"center"}
+                            borderRadius={"base"}
+                            shadow={"lg"}
+                        />
+                    </ModalContent>
+                </Modal>
+            )}
+        </>
+    );
+};
+
+const SidebarItem = ({ icon, title, onClick, loading }) => {
+    return (
+        <Stack
+            direction={"row"}
+            align={"center"}
+            justifyContent={"space-between"}
+            onClick={onClick}
+            cursor={"pointer"}
+            p={2}
+            rounded={"md"}
+            _hover={{
+                bg: useColorModeValue("gray.50", "gray.900"),
+                color: "red.500",
+            }}
+            transition={"all .1s ease"}
+        >
+            <Stack direction={"row"} align={"center"} spacing={4}>
+                {loading ? (
+                    <>
+                        <Skeleton height={"20px"} width={"20px"} />
+                        <Skeleton height={"20px"} width={"60px"} />
+                    </>
+                ) : (
+                    <>
+                        <Icon as={icon} w={5} h={5} />
+                        <Text fontWeight={500}>{title}</Text>
+                    </>
+                )}
+            </Stack>
+        </Stack>
+    );
+};
