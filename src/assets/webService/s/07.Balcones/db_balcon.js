b6cf01b63f9e7f18a9071ca38703b4a463579ef9
Slinkter <luis.j.cueva@gmail.com>
2024-08-03 10:17:21 -0500

    sabado : init

--- src/assets/webService/s/07.Balcones/db_balcon.js
+++ src/assets/webService/s/07.Balcones/db_balcon.js
@@ -7,38 +7,38 @@ import image05 from "./IMG_2091.jpeg";
 import image06 from "./IMG_2097.jpeg";
 
 const listBalcon = {
-  balcon: [
-    {
-      id: 1,
-      image: image01,
-      name: "techo",
-    },
-    {
-      id: 2,
-      image: image02,
-      name: "techo",
-    },
-    {
-      id: 3,
-      image: image03,
-      name: "techo",
-    },
-    {
-      id: 4,
-      image: image04,
-      name: "techo",
-    },
-    {
-      id: 5,
-      image: image05,
-      name: "techo",
-    },
-    {
-      id: 6,
-      image: image06,
-      name: "techo",
-    },
-  ],
+    balcon: [
+        {
+            id: 1,
+            image: image01,
+            name: "techo",
+        },
+        {
+            id: 2,
+            image: image02,
+            name: "techo",
+        },
+        {
+            id: 3,
+            image: image03,
+            name: "techo",
+        },
+        {
+            id: 4,
+            image: image04,
+            name: "techo",
+        },
+        {
+            id: 5,
+            image: image05,
+            name: "techo",
+        },
+        {
+            id: 6,
+            image: image06,
+            name: "techo",
+        },
+    ],
 };
 
 export { listBalcon };
--- src/assets/webService/s/12.PuertaS/db_puertas.js
+++ src/assets/webService/s/12.PuertaS/db_puertas.js
@@ -7,38 +7,38 @@ import image05 from "./IMG_9738.jpeg";
 import image06 from "./IMG_9739.jpeg";
 
 const listPuertas = {
-  puertas: [
-    {
-      id: 1,
-      image: image01,
-      name: "techo",
-    },
-    {
-      id: 2,
-      image: image02,
-      name: "techo",
-    },
-    {
-      id: 3,
-      image: image03,
-      name: "techo",
-    },
-    {
-      id: 4,
-      image: image04,
-      name: "techo",
-    },
-    {
-      id: 5,
-      image: image05,
-      name: "techo",
-    },
-    {
-      id: 6,
-      image: image06,
-      name: "techo",
-    },
-  ],
+    puertas: [
+        {
+            id: 1,
+            image: image01,
+            name: "techo",
+        },
+        {
+            id: 2,
+            image: image02,
+            name: "techo",
+        },
+        {
+            id: 3,
+            image: image03,
+            name: "techo",
+        },
+        {
+            id: 4,
+            image: image04,
+            name: "techo",
+        },
+        {
+            id: 5,
+            image: image05,
+            name: "techo",
+        },
+        {
+            id: 6,
+            image: image06,
+            name: "techo",
+        },
+    ],
 };
 
 export { listPuertas };
