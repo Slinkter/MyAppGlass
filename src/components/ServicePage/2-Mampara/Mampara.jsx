41f5bf57eb6083773b7d548661ac7127df41dbd6
Slinkter <luis.j.cueva@gmail.com>
2024-09-05 11:36:49 -0500

    update

--- src/components/ServicePage/2-Mampara/Mampara.jsx
+++ src/components/ServicePage/2-Mampara/Mampara.jsx
@@ -17,7 +17,9 @@ import {
     useMediaQuery,
     SkeletonCircle,
 } from "@chakra-ui/react";
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+import { CheckIcon } from "@heroicons/react/24/solid";
 import { listMampara } from "../../../assets/webService/s/02.Mampara/db_mampara";
 window.document.title = "Mampara";
 
@@ -44,7 +46,7 @@ const Mampara = () => {
         >
             <Box
                 w={{ base: "full", md: "20vw" }}
-                h={{ base: "auto", md: "80vh" }}
+                h={{ base: "auto", md: "85vh" }}
                 m={{ base: "20px" }}
                 p={4}
                 bg={useColorModeValue("white", "gray.800")}
@@ -54,7 +56,7 @@ const Mampara = () => {
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
                     boxShadow: "xl",
-                    borderColor: "red.200",
+                    borderColor: "gray.300",
                 }}
             >
                 <Box mb={1} p={3} opacity={loading ? 0 : 1}>
@@ -68,21 +70,60 @@ const Mampara = () => {
                 </Box>
                 <Stack spacing={1}>
                     <SidebarItem
-                        icon={PaperAirplaneIcon}
-                        title="Nova"
+                        icon={ChevronRightIcon}
+                        label="Sistema Nova"
+                        isActive={open === 0} // Activo si open es 0
                         onClick={() => setOpen(0)}
                         loading={loading}
                     />
                     <SidebarItem
-                        icon={PaperAirplaneIcon}
-                        title="serie 25"
+                        icon={ChevronRightIcon}
+                        label="Sistema Serie 25"
+                        isActive={open === 1}
                         onClick={() => setOpen(1)}
+                        loading={loading}
+                    />
+                </Stack>
+                <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                    <Text
+                        fontSize="2xl"
+                        fontWeight="bold"
+                        color={useColorModeValue("gray.700", "gray.200")}
+                    >
+                        CARACTERÍSTICA
+                    </Text>
+                </Box>
+                <Stack spacing={1}>
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Color : Incoro | Bronce"
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Tipo : Templado | Crudo"
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Aluminio : Natural | Negro"
+                        loading={loading}
+                    />
+
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Diseño : Lamina | Logo"
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Espesor : 8 mm "
                         loading={loading}
                     />
                 </Stack>
             </Box>
             <Box
-                h={{ base: "auto", md: "80vh" }}
+                h={{ base: "auto", md: "85vh" }}
                 w={{ base: "full", md: "100vw" }}
                 mx={{ base: "20px" }}
                 p={4}
@@ -92,7 +133,7 @@ const Mampara = () => {
                 border={"1px solid "}
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
-                    borderColor: "red.200",
+                    borderColor: "gray.300",
                     boxShadow: "xl",
                 }}
             >
@@ -108,7 +149,7 @@ const Mampara = () => {
 
 export default Mampara;
 
-const SidebarItem = ({ icon, title, onClick, loading }) => {
+const SidebarItem = ({ icon, label, onClick, isActive, loading }) => {
     return (
         <Stack
             direction="row"
@@ -118,11 +159,31 @@ const SidebarItem = ({ icon, title, onClick, loading }) => {
             cursor="pointer"
             p={2}
             rounded="md"
+            bg={isActive ? "red.100" : "transparent"} // Cambia el fondo si está activo
+            color={isActive ? "red.500" : "inherit"} // Cambia el color si está activo
             _hover={{
                 bg: useColorModeValue("gray.50", "gray.900"),
                 color: "red.500",
             }}
         >
+            {loading ? (
+                <Stack direction="row" align="center" spacing={4}>
+                    <SkeletonCircle w={5} h={5} />
+                    <Skeleton height="20px" w={"140px"} />
+                </Stack>
+            ) : (
+                <Stack direction="row" align="center" spacing={4}>
+                    <Icon w={5} h={5} as={icon} />
+                    <Text fontWeight={600}>{label}</Text>
+                </Stack>
+            )}
+        </Stack>
+    );
+};
+
+const CharItem = ({ icon, label, loading }) => {
+    return (
+        <Stack direction="row" align="center" justify="space-between" p={2}>
             {loading && (
                 <Stack direction="row" align="center" spacing={4}>
                     <SkeletonCircle w={5} h={5} />
@@ -133,7 +194,7 @@ const SidebarItem = ({ icon, title, onClick, loading }) => {
             {!loading && (
                 <Stack direction="row" align="center" spacing={4}>
                     <Icon w={5} h={5} as={icon} />
-                    <Text fontWeight={600}>{title}</Text>
+                    <Text fontWeight={600}>{label}</Text>
                 </Stack>
             )}
         </Stack>
@@ -149,7 +210,11 @@ const Gallery = ({ images, loading }) => {
 
     return (
         <>
-            <Grid templateColumns={`repeat(${responsiveColumns}, 1fr)`} gap={4}>
+            <Grid
+                templateColumns={`repeat(${responsiveColumns}, 1fr)`}
+                gap={4}
+                p={2}
+            >
                 {loading &&
                     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12].map((_, idx) => (
                         <GridItem key={idx}>
@@ -170,12 +235,20 @@ const Gallery = ({ images, loading }) => {
                                 src={src.image}
                                 rounded="lg"
                                 objectFit={"cover"}
-                                transition="opacity 0.5s ease-in-out"
+                                transition="all .2s ease-in-out"
                                 opacity={loading ? 0 : 1}
                                 cursor={"pointer"}
+                                shadow={"base"}
                                 onClick={() => {
                                     setSelectedImage(src.image);
                                     setIsOpen(true);
+                                }}
+                                _hover={{
+                                    shadow: "lg",
+                                    transform: {
+                                        base: "scale(1.00)",
+                                        md: "scale(1.02)",
+                                    },
                                 }}
                             />
                         </GridItem>
