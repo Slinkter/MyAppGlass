16f34dc89e482cf5a9f14d74b2774e4aadcec7c6
Slinkter <luis.j.cueva@gmail.com>
2024-09-06 12:38:00 -0500

    update

--- src/components/ServicePage/3-Ducha/Ducha.jsx
+++ src/components/ServicePage/3-Ducha/Ducha.jsx
@@ -14,65 +14,104 @@ import {
     ModalCloseButton,
     Skeleton,
     useMediaQuery,
-    Container,
     Heading,
-    Flex,
+    SkeletonCircle,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { CheckIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import { listDucha } from "../../../assets/webService/s/03.Ducha/db_ducha";
-
+window.document.title =
+    "Vidrieria en La Molina instalación de ventanas, mamparas, puertas de ducha y más. Servicios de mantenimiento y calidad garantizada. tef. 996-537-435";
 const Ducha = () => {
-    window.document.title = "Ducha";
     const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState(0);
 
-    setTimeout(() => {
-        setLoading(false);
-    }, 1000);
+    useEffect(() => {
+        setLoading(true);
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [open]);
 
     return (
-        <Box display={"flex"} flexDir={{ base: "column", md: "row" }}>
+        <Box
+            display="flex"
+            flexDirection={{ base: "column", md: "row" }}
+            justifyContent={"center"}
+            alignItems={"center"}
+            p={8}
+        >
             <Box
-                ml="2rem"
-                mt="2rem"
-                mr={{ base: "2rem", md: "2rem" }}
+                w={{ base: "full", md: "30vw" }}
+                h={{ base: "auto", md: "85vh" }}
+                m={{ base: "20px" }}
                 p={4}
                 bg={useColorModeValue("white", "gray.800")}
-                h={{ base: "auto", md: "80vh" }}
-                w={{ base: "full-20rem", md: "15rem" }}
                 rounded="lg"
                 boxShadow="md"
                 border={"1px solid "}
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
+                    boxShadow: "xl",
                     borderColor: "gray.300",
-                    boxShadow: "lg",
                 }}
             >
-                <Box mb={2} p={4}>
-                    <Heading as="h2" fontWeight="600" color="red.500" mt={4}>
-                        Puerta de ducha
-                    </Heading>
+                <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                    <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                        <Text
+                            fontSize="2xl"
+                            fontWeight="bold"
+                            color={useColorModeValue("gray.700", "gray.200")}
+                        >
+                            PUERTA DE DUCHA
+                        </Text>
+                    </Box>
 
                     <Stack spacing={1}>
                         <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Vidrio templado de 8mm"
+                            icon={ChevronRightIcon}
+                            label="Sistema KIT"
                             loading={loading}
                         />
                         <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Accesorios de acero cromado"
+                            icon={ChevronRightIcon}
+                            label="Sistema Acrilico"
                             loading={loading}
                         />
-                        <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Arenado con diseño"
+                    </Stack>
+                    <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                        <Text
+                            fontSize="2xl"
+                            fontWeight="bold"
+                            color={useColorModeValue("gray.700", "gray.200")}
+                        >
+                            CARACTERÍSTICA
+                        </Text>
+                    </Box>
+
+                    <Stack>
+                        <CharItem
+                            icon={CheckIcon}
+                            label="Color : Incoro | Bronce"
                             loading={loading}
                         />
-                        <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="tiempc "
+                        <CharItem
+                            icon={CheckIcon}
+                            label="Tipo : Templado"
+                            loading={loading}
+                        />
+                        <CharItem
+                            icon={CheckIcon}
+                            label="Aluminio : Natural | Negro"
+                            loading={loading}
+                        />
+                        <CharItem
+                            icon={CheckIcon}
+                            label="Espesor : 8 mm "
                             loading={loading}
                         />
                     </Stack>
@@ -80,23 +119,18 @@ const Ducha = () => {
             </Box>
 
             <Box
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-                mt="2rem"
-                mr="2rem"
-                mb="2rem"
-                ml={{ base: "2rem", md: "0rem" }}
+                h={{ base: "auto", md: "85vh" }}
+                w={{ base: "full", md: "100vw" }}
+                mx={{ base: "20px" }}
                 p={4}
                 bg={useColorModeValue("white", "gray.800")}
-                h={{ base: "auto", md: "80vh" }}
                 rounded="lg"
                 boxShadow="md"
                 border={"1px solid "}
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
                     borderColor: "gray.300",
-                    boxShadow: "lg",
+                    boxShadow: "xl",
                 }}
             >
                 <Gallery images={listDucha.kit} loading={loading} />
@@ -107,45 +141,79 @@ const Ducha = () => {
 
 export default Ducha;
 
+const CharItem = ({ icon, label, loading }) => {
+    return (
+        <Stack direction="row" align="center" justify="space-between" p={2}>
+            {loading && (
+                <Stack direction="row" align="center" spacing={4}>
+                    <SkeletonCircle w={5} h={5} />
+                    <Skeleton height="20px" w={"140px"} />
+                </Stack>
+            )}
+
+            {!loading && (
+                <Stack direction="row" align="center" spacing={4}>
+                    <Icon w={5} h={5} as={icon} />
+                    <Text>{label}</Text>
+                </Stack>
+            )}
+        </Stack>
+    );
+};
+
 const Gallery = ({ images, loading }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
-    const [isMobile] = useMediaQuery();
-    const resposiveColmns = useBreakpointValue({ base: 1, md: 4 });
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+    const responsiveColumns = useBreakpointValue({ base: 1, md: 5 });
     const onClose = () => setIsOpen(false);
 
     console.log("images", images);
     return (
         <>
-            <Grid templateColumns={`repeat(${resposiveColmns} ,1fr)`} gap={4}>
-                {images.map((item) => (
-                    <GridItem key={item.id}>
-                        {loading ? (
+            <Grid
+                templateColumns={`repeat(${responsiveColumns}, 1fr)`}
+                gap={4}
+                p={2}
+            >
+                {loading &&
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12].map((_, idx) => (
+                        <GridItem key={idx}>
                             <Skeleton
-                                w={{ base: "full", md: "18vw" }}
-                                minH={"24vh"}
-                                h={"24vh"}
-                                rounded={"lg"}
+                                w="full"
+                                h={{ base: "", md: "24vh" }}
+                                rounded="lg"
                             />
-                        ) : (
+                        </GridItem>
+                    ))}
+
+                {!loading &&
+                    images.map((src, index) => (
+                        <GridItem key={index}>
                             <Image
-                                w={{ base: "full", md: "18vw" }}
-                                minH={"24vh"}
-                                h={"24vh"}
-                                rounded={"lg"}
-                                src={item.image}
+                                w="full"
+                                h={{ base: "44vh", md: "24vh" }}
+                                src={src.image}
+                                rounded="lg"
                                 objectFit={"cover"}
-                                mb={{ base: 4, md: 0 }}
-                                shadow={"base"}
+                                transition="all .2s ease-in-out"
+                                opacity={loading ? 0 : 1}
                                 cursor={"pointer"}
+                                shadow={"base"}
                                 onClick={() => {
-                                    setSelectedImage(item.image);
+                                    setSelectedImage(src.image);
                                     setIsOpen(true);
                                 }}
+                                _hover={{
+                                    shadow: "lg",
+                                    transform: {
+                                        base: "scale(1.00)",
+                                        md: "scale(1.02)",
+                                    },
+                                }}
                             />
-                        )}
-                    </GridItem>
-                ))}
+                        </GridItem>
+                    ))}
             </Grid>
             {!isMobile && (
                 <Modal
@@ -172,35 +240,34 @@ const Gallery = ({ images, loading }) => {
     );
 };
 
-const SidebarItem = ({ icon, title, onClick, loading }) => {
+const SidebarItem = ({ icon, label, onClick, isActive, loading }) => {
     return (
         <Stack
-            direction={"row"}
-            align={"center"}
-            justifyContent={"space-between"}
+            direction="row"
+            align="center"
+            justify="space-between"
             onClick={onClick}
-            cursor={"pointer"}
+            cursor="pointer"
             p={2}
-            rounded={"md"}
+            rounded="md"
+            bg={isActive ? "red.100" : "transparent"} // Cambia el fondo si está activo
+            color={isActive ? "red.500" : "inherit"} // Cambia el color si está activo
             _hover={{
                 bg: useColorModeValue("gray.50", "gray.900"),
                 color: "red.500",
             }}
-            transition={"all .1s ease"}
         >
-            <Stack direction={"row"} align={"center"} spacing={4}>
-                {loading ? (
-                    <>
-                        <Skeleton height={"20px"} width={"20px"} />
-                        <Skeleton height={"20px"} width={"60px"} />
-                    </>
-                ) : (
-                    <>
-                        <Icon as={icon} w={5} h={5} />
-                        <Text fontWeight={500}>{title}</Text>
-                    </>
-                )}
-            </Stack>
+            {loading ? (
+                <Stack direction="row" align="center" spacing={4}>
+                    <SkeletonCircle w={5} h={5} />
+                    <Skeleton height="20px" w={"140px"} />
+                </Stack>
+            ) : (
+                <Stack direction="row" align="center" spacing={4}>
+                    <Icon w={5} h={5} as={icon} />
+                    <Text>{label}</Text>
+                </Stack>
+            )}
         </Stack>
     );
 };
--- src/components/ServicePage/5-Baranda/Baranda.jsx
+++ src/components/ServicePage/5-Baranda/Baranda.jsx
@@ -17,80 +17,118 @@ import {
     Container,
     Heading,
     Flex,
+    SkeletonCircle,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { CheckIcon } from "@heroicons/react/24/solid";
 import { listBaranda } from "../../../assets/webService/s/06.Baranda/db_baranda";
 
 const Baranda = () => {
-    window.document.title = "Baranda";
     const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState(0);
 
-    setTimeout(() => {
-        setLoading(false);
-    }, 1000);
+    //
+    useEffect(() => {
+        setLoading(true);
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [open]);
+
     return (
-        <Box display={"flex"} flexDir={{ base: "column", md: "row" }}>
+        <Box
+            display="flex"
+            flexDirection={{ base: "column", md: "row" }}
+            justifyContent={"center"}
+            alignItems={"center"}
+            p={8}
+        >
             <Box
-                ml="2rem"
-                mt="2rem"
-                mr={{ base: "2rem", md: "2rem" }}
+                w={{ base: "full", md: "25vw" }}
+                h={{ base: "auto", md: "85vh" }}
+                m={{ base: "20px" }}
                 p={4}
                 bg={useColorModeValue("white", "gray.800")}
-                h={{ base: "auto", md: "80vh" }}
-                w={{ base: "full-20rem", md: "15rem" }}
                 rounded="lg"
                 boxShadow="md"
                 border={"1px solid "}
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
+                    boxShadow: "xl",
                     borderColor: "gray.300",
-                    boxShadow: "lg",
                 }}
             >
-                <Box mb={2} p={4}>
-                    <Heading as="h2" fontWeight="600" color="red.500" mt={4}>
-                        Barandas de acero
-                    </Heading>
-
-                    <Stack spacing={1}>
-                        <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Vidrio templado de 8mm"
-                            loading={loading}
-                        />
-                        <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Accesorios de acero cromado"
-                            loading={loading}
-                        />
-                        <SidebarItem
-                            icon={PaperAirplaneIcon}
-                            title="Cristal Empavonado "
-                            loading={loading}
-                        />
-                    </Stack>
+                <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                    <Text
+                        fontSize="2xl"
+                        fontWeight="bold"
+                        color={useColorModeValue("gray.700", "gray.200")}
+                    >
+                        BARANDAS
+                    </Text>
                 </Box>
-            </Box>
+                <Stack spacing={1}>
+                    <SidebarItem
+                        icon={ChevronRightIcon}
+                        label="Sistema "
+                        loading={loading}
+                    />
+                </Stack>
 
+                <Box mb={1} p={3} opacity={loading ? 0 : 1}>
+                    <Text
+                        fontSize="2xl"
+                        fontWeight="bold"
+                        color={useColorModeValue("gray.700", "gray.200")}
+                    >
+                        CARACTERÍSTICA
+                    </Text>
+                </Box>
+                <Stack spacing={1}>
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Material : Acero "
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Color : Incoro | Bronce"
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Arenado : Lamina"
+                        loading={loading}
+                    />
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Tipo : Templado "
+                        loading={loading}
+                    />
+
+                    <CharItem
+                        icon={CheckIcon}
+                        label="Espesor : 8 mm "
+                        loading={loading}
+                    />
+                </Stack>
+            </Box>
             <Box
-                display={"flex"}
-                justifyContent={"start"}
-                alignItems={"start"}
-                mt="2rem"
-                mr="2rem"
-                mb="2rem"
-                ml={{ base: "2rem", md: "0rem" }}
+                h={{ base: "auto", md: "85vh" }}
+                w={{ base: "full", md: "100vw" }}
+                mx={{ base: "20px" }}
                 p={4}
                 bg={useColorModeValue("white", "gray.800")}
-                h={{ base: "auto", md: "80vh" }}
                 rounded="lg"
                 boxShadow="md"
                 border={"1px solid "}
                 borderColor={useColorModeValue("gray.200", "gray.600")}
                 _hover={{
                     borderColor: "gray.300",
-                    boxShadow: "lg",
+                    boxShadow: "xl",
                 }}
             >
                 <Gallery images={listBaranda.baranda} loading={loading} />
@@ -101,100 +139,133 @@ const Baranda = () => {
 
 export default Baranda;
 
+const SidebarItem = ({ icon, label, onClick, isActive, loading }) => {
+    return (
+        <Stack
+            direction="row"
+            align="center"
+            justify="space-between"
+            onClick={onClick}
+            cursor="pointer"
+            p={2}
+            rounded="md"
+            bg={isActive ? "red.100" : "transparent"} // Cambia el fondo si está activo
+            color={isActive ? "red.500" : "inherit"} // Cambia el color si está activo
+            _hover={{
+                bg: useColorModeValue("gray.50", "gray.900"),
+                color: "red.500",
+            }}
+        >
+            {loading ? (
+                <Stack direction="row" align="center" spacing={4}>
+                    <SkeletonCircle w={5} h={5} />
+                    <Skeleton height="20px" w={"140px"} />
+                </Stack>
+            ) : (
+                <Stack direction="row" align="center" spacing={4}>
+                    <Icon w={5} h={5} as={icon} />
+                    <Text>{label}</Text>
+                </Stack>
+            )}
+        </Stack>
+    );
+};
+const CharItem = ({ icon, label, loading }) => {
+    return (
+        <Stack direction="row" align="center" justify="space-between" p={2}>
+            {loading && (
+                <Stack direction="row" align="center" spacing={4}>
+                    <SkeletonCircle w={5} h={5} />
+                    <Skeleton height="20px" w={"140px"} />
+                </Stack>
+            )}
+
+            {!loading && (
+                <Stack direction="row" align="center" spacing={4}>
+                    <Icon w={5} h={5} as={icon} />
+                    <Text>{label}</Text>
+                </Stack>
+            )}
+        </Stack>
+    );
+};
+
 const Gallery = ({ images, loading }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
-    const [isMobile] = useMediaQuery();
-    const resposiveColmns = useBreakpointValue({ base: 1, md: 4 });
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+    const responsiveColumns = useBreakpointValue({ base: 1, md: 5 });
     const onClose = () => setIsOpen(false);
 
-    console.log("images", images);
     return (
         <>
-            <Grid templateColumns={`repeat(${resposiveColmns} ,1fr)`} gap={4}>
-                {images.map((item) => (
-                    <GridItem key={item.id}>
-                        {loading ? (
+            <Grid
+                templateColumns={`repeat(${responsiveColumns}, 1fr)`}
+                gap={4}
+                p={2}
+            >
+                {loading &&
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12].map((_, idx) => (
+                        <GridItem key={idx}>
                             <Skeleton
-                                w={{ base: "full", md: "18vw" }}
-                                minH={"24vh"}
-                                h={"24vh"}
-                                rounded={"lg"}
+                                w="full"
+                                h={{ base: "", md: "24vh" }}
+                                rounded="lg"
                             />
-                        ) : (
+                        </GridItem>
+                    ))}
+
+                {!loading &&
+                    images.map((src, index) => (
+                        <GridItem key={index}>
                             <Image
-                                w={{ base: "full", md: "18vw" }}
-                                minH={"24vh"}
-                                h={"24vh"}
-                                rounded={"lg"}
-                                src={item.image}
+                                w="full"
+                                h={{ base: "44vh", md: "24vh" }}
+                                src={src.image}
+                                rounded="lg"
                                 objectFit={"cover"}
-                                mb={{ base: 4, md: 0 }}
-                                shadow={"base"}
+                                transition="all .2s ease-in-out"
+                                opacity={loading ? 0 : 1}
                                 cursor={"pointer"}
+                                shadow={"base"}
                                 onClick={() => {
-                                    setSelectedImage(item.image);
+                                    setSelectedImage(src.image);
                                     setIsOpen(true);
                                 }}
+                                _hover={{
+                                    shadow: "lg",
+                                    transform: {
+                                        base: "scale(1.00)",
+                                        md: "scale(1.02)",
+                                    },
+                                }}
                             />
-                        )}
-                    </GridItem>
-                ))}
+                        </GridItem>
+                    ))}
             </Grid>
+
             {!isMobile && (
                 <Modal
                     isOpen={isOpen}
                     onClose={onClose}
                     isCentered
                     motionPreset="slideInBottom"
+                    size="xl"
                 >
-                    <ModalOverlay backdropFilter={"blur(10px)"} />
+                    <ModalOverlay backdropFilter="blur(10px)" />
                     <ModalContent>
                         <ModalCloseButton />
                         <Image
                             src={selectedImage}
                             alt={selectedImage}
-                            objectFit={"cover"}
-                            align={"center"}
-                            borderRadius={"base"}
-                            shadow={"lg"}
+                            objectFit="cover"
+                            borderRadius="base"
+                            shadow="lg"
+                            maxH="100vh"
                         />
                     </ModalContent>
                 </Modal>
             )}
         </>
-    );
-};
-
-const SidebarItem = ({ icon, title, onClick, loading }) => {
-    return (
-        <Stack
-            direction={"row"}
-            align={"center"}
-            justifyContent={"space-between"}
-            onClick={onClick}
-            cursor={"pointer"}
-            p={2}
-            rounded={"md"}
-            _hover={{
-                bg: useColorModeValue("gray.50", "gray.900"),
-                color: "red.500",
-            }}
-            transition={"all .1s ease"}
-        >
-            <Stack direction={"row"} align={"center"} spacing={4}>
-                {loading ? (
-                    <>
-                        <Skeleton height={"20px"} width={"20px"} />
-                        <Skeleton height={"20px"} width={"60px"} />
-                    </>
-                ) : (
-                    <>
-                        <Icon as={icon} w={5} h={5} />
-                        <Text fontWeight={500}>{title}</Text>
-                    </>
-                )}
-            </Stack>
-        </Stack>
     );
 };
