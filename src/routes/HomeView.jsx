ae28cb5bc00529a1632b7a2632a16608c1370b97
Slinkter <luis.j.cueva@gmail.com>
2024-11-03 16:51:51 -0500

    update

--- src/routes/HomeView.jsx
+++ src/routes/HomeView.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Box, Button, useColorMode } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Tienda from "../components/HomePage/Tienda";
 import Feature from "../components/HomePage/Feature";
 import Clients from "../components/HomePage/Clients";
 import LandPage from "../components/HomePage/LandPage";
-window.document.title =
-    "Vidrieria en La Molina instalación de ventanas, mamparas, puertas de ducha y más. Servicios de mantenimiento y calidad garantizada. tef. 996-537-435";
+
 const HomeView = () => {
     return (
         <Box>
