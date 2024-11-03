361e882ce395c472370637e708153e0d0cdadf69
Slinkter <luis.j.cueva@gmail.com>
2024-11-03 16:51:51 -0500

    update

--- src/routes/UsView.jsx
+++ src/routes/UsView.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import Tienda from "../components/HomePage/Tienda";
+import Feature from "../components/HomePage/Feature";
+import Clients from "../components/HomePage/Clients";
+import LandPage from "../components/HomePage/LandPage";
 
 const UsView = () => {
-  return <div>UsView</div>;
+    return (
+        <Box>
+            <LandPage />
+            <Clients />
+            <Feature />
+            <Tienda />
+        </Box>
+    );
 };
 
 export default UsView;
