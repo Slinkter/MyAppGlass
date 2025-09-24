b8923be93708e9d0ca778b6c3a5c119fac992586
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-09-24 12:29:58 -0500

    [ADD]-PLUGIN IMAGE OPTIMIZER

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        ViteImageOptimizer({
+            /* Opciones de configuración para la optimización */
+
+            // Configuración para PNG
+            png: {
+                quality: 85, // Calidad del 0 al 100
+            },
+            // Configuración para JPEG
+            jpeg: {
+                quality: 85, // Calidad del 0 al 100
+            },
+            // Configuración para JPG
+            jpg: {
+                quality: 85, // Calidad del 0 al 100
+            },
+            // Crear versiones WebP de tus imágenes
+            webp: {
+                quality: 85, // Calidad del 0 al 100
+            },
+        }),
+    ],
 });
-// pre modificacion para image optimizer
