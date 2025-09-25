31462648578e0ea60ac9a759d541949d262d6fdc
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-09-25 12:03:41 -0500

    [MODIFID]-NAVBAR.JSX

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -46,7 +46,8 @@ export default function WithSubnavigation() {
     const bgColor = useColorModeValue(colorWhite, colorBlack);
 
     return (
-        <Box>
+        <>
+            {/* desktop */}
             <Flex
                 minH={"50px"}
                 py={{ base: 2 }}
@@ -56,10 +57,10 @@ export default function WithSubnavigation() {
                 borderColor={bgColor}
                 align={"center"}
             >
-                <Flex
+                <Flex // 1.mobil
+                    display={{ base: "flex", md: "none" }}
                     flex={{ base: 1, md: "auto" }}
                     ml={{ base: -2 }}
-                    display={{ base: "flex", md: "none" }}
                 >
                     <IconButton
                         onClick={onToggle}
@@ -74,19 +75,20 @@ export default function WithSubnavigation() {
                         variant={"ghost"}
                     />
                 </Flex>
-                <Flex
+                <Flex // 2.desktop
                     flex={{ base: 1 }}
-                    justify={{ base: "center", md: "center" }}
+                    justifyContent={"center"}
+                    alignItems={"center"}
                 >
                     <Flex display={{ base: "none", md: "flex" }}>
                         <DesktopNav />
                     </Flex>
                 </Flex>
-                <Flex
+                <Flex // 3.Theme
                     w={10}
                     h={10}
-                    align={"center"}
                     justify={"center"}
+                    align={"center"}
                     rounded={"full"}
                     bg={bgColor}
                     onClick={toggleColorMode}
@@ -94,11 +96,11 @@ export default function WithSubnavigation() {
                     {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
                 </Flex>
             </Flex>
-
+            {/* movil */}
             <Collapse in={isOpen} animateOpacity>
                 <MobileNav />
             </Collapse>
-        </Box>
+        </>
     );
 }
 
@@ -107,103 +109,30 @@ export default function WithSubnavigation() {
 const DesktopNav = () => {
     const linkColor = useColorModeValue("gray.600", "gray.200");
     const linkHoverColor = useColorModeValue("red.400", "red.600");
-    const popoverContentBgColor = useColorModeValue("white", "gray.800");
 
     return (
         <Stack direction={"row"} spacing={4}>
             {NAV_ITEMS.map((navItem) => (
-                <Box key={navItem.label}>
-                    <Popover trigger={"hover"} placement={"bottom-start"}>
-                        <PopoverTrigger>
-                            <Box
-                                as="a"
-                                p={2}
-                                href={navItem.href ?? "#"}
-                                fontSize={"md"}
-                                fontWeight={600}
-                                color={linkColor}
-                                _hover={{
-                                    textDecoration: "none",
-                                    color: linkHoverColor,
-                                }}
-                            >
-                                {navItem.label}
-                            </Box>
-                        </PopoverTrigger>
-
-                        {navItem.children && (
-                            <PopoverContent
-                                border={"1px solid"}
-                                borderColor={"gray.100"}
-                                boxShadow={"xl"}
-                                bg={popoverContentBgColor}
-                                p={4}
-                                rounded={"xl"}
-                                minW={"sm"}
-                            >
-                                <Stack>
-                                    {navItem.children.map((child) => (
-                                        <DesktopSubNav
-                                            key={child.label}
-                                            {...child}
-                                        />
-                                    ))}
-                                </Stack>
-                            </PopoverContent>
-                        )}
-                    </Popover>
+                <Box
+                    key={navItem.label}
+                    as="a"
+                    p={2}
+                    href={navItem.href ?? "#"}
+                    fontSize={"md"}
+                    fontWeight={600}
+                    color={linkColor}
+                    _hover={{
+                        textDecoration: "none",
+                        color: linkHoverColor,
+                    }}
+                >
+                    {navItem.label}
                 </Box>
             ))}
         </Stack>
     );
 };
-const DesktopSubNav = ({ label, href, subLabel }) => {
-    return (
-        <Box
-            as="a"
-            href={href}
-            role={"group"}
-            display={"block"}
-            p={2}
-            rounded={"md"}
-            _hover={{ bg: useColorModeValue("gray.50", "gray.900") }}
-        >
-            <Stack direction={"row"} align={"center"}>
-                <Box>
-                    <Text
-                        transition={"all .3s ease"}
-                        _groupHover={{
-                            color: useColorModeValue("red.500", "red.300"),
-                        }}
-                        fontWeight={500}
-                    >
-                        {label}
-                    </Text>
-                    <Text fontSize={"sm"}>{subLabel}</Text>
-                </Box>
-                <Flex
-                    transition={"all .3s ease"}
-                    transform={"translateX(-10px)"}
-                    opacity={0}
-                    _groupHover={{
-                        opacity: "100%",
-                        transform: "translateX(0)",
-                    }}
-                    justify={"flex-end"}
-                    align={"center"}
-                    flex={1}
-                >
-                    <Icon
-                        color={useColorModeValue("red.500", "red.300")}
-                        w={5}
-                        h={5}
-                        as={ChevronRightIcon}
-                    />
-                </Flex>
-            </Stack>
-        </Box>
-    );
-};
+
 /*  */
 const MobileNav = () => {
     return (
@@ -222,7 +151,7 @@ const MobileNav = () => {
 /*  */
 
 const MobileNavItem = ({ label, children, href }) => {
-    const { isOpen, onToggle } = useDisclosure();
+    const { onToggle } = useDisclosure();
 
     return (
         <Stack spacing={4} onClick={children && onToggle}>
@@ -242,43 +171,7 @@ const MobileNavItem = ({ label, children, href }) => {
                 >
                     {label}
                 </Text>
-                {children && (
-                    <Icon
-                        as={ChevronDownIcon}
-                        transition={"all .25s ease-in-out"}
-                        transform={isOpen ? "rotate(180deg)" : ""}
-                        w={6}
-                        h={6}
-                    />
-                )}
             </Box>
-
-            <Collapse
-                in={isOpen}
-                animateOpacity
-                style={{ marginTop: "0!important" }}
-            >
-                <Stack
-                    mt={2}
-                    pl={4}
-                    borderLeft={1}
-                    borderStyle={"solid"}
-                    borderColor={useColorModeValue("gray.200", "gray.700")}
-                    align={"start"}
-                >
-                    {children &&
-                        children.map((child) => (
-                            <Box
-                                as="a"
-                                key={child.label}
-                                py={2}
-                                href={child.href}
-                            >
-                                {child.label}
-                            </Box>
-                        ))}
-                </Stack>
-            </Collapse>
         </Stack>
     );
 };
