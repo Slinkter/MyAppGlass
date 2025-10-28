cf38b91a349836ba460131b372ae55b6ce04ddae
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-10-27 22:14:48 -0500

    revision 08

---
+++ src/components/common/ColorModeSwitcher.jsx
@@ -0,0 +1,28 @@
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const ColorModeSwitcher = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const bgColor = useColorModeValue("gray.200", "blackAlpha.500");
+
+    return (
+        <IconButton
+            size="md"
+            fontSize="lg"
+            aria-label={`Switch to ${colorMode === "light" ? "dark" : "light"} mode`}
+            variant="ghost"
+            color="current"
+            marginLeft="2"
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            bg={bgColor}
+            rounded="full"
+            w={10}
+            h={10}
+            justify={"center"}
+            align={"center"}
+        />
+    );
+};
+
+export default ColorModeSwitcher;
---
+++ src/data/nav-items.js
@@ -0,0 +1,16 @@
+const NAV_ITEMS = [
+    {
+        label: "Inicio",
+        href: "/",
+    },
+    {
+        label: "Servicios",
+        href: "/servicios",
+    },
+    {
+        label: "Proyectos",
+        href: "/proyectos",
+    },
+];
+
+export default NAV_ITEMS;
