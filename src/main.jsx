7bfa0b11f1eca2f0f0d85d714eb968cac76350c0
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-11-26 17:09:14 -0500

    antigravity

--- src/main.jsx
+++ src/main.jsx
@@ -12,22 +12,20 @@ import "./styles/global.css";
 
 // El fallback de Suspense se mostrará mientras se carga el código del componente diferido.
 const suspenseFallback = (
-    <Center h="100vh">
-        <Spinner size="xl" />
-    </Center>
+  <Center h="100vh">
+    <Spinner size="xl" />
+  </Center>
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <HelmetProvider>
-            <ChakraProvider theme={theme}>
-                <ColorModeScript
-                    initialColorMode={theme.config.initialColorMode}
-                />
-                <React.Suspense fallback={suspenseFallback}>
-                    <RouterProvider router={router} />
-                </React.Suspense>
-            </ChakraProvider>
-        </HelmetProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <HelmetProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <React.Suspense fallback={suspenseFallback}>
+          <RouterProvider router={router} />
+        </React.Suspense>
+      </ChakraProvider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
