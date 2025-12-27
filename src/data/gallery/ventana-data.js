61b604b60d63a02ea781cd0d98f1221889899eb5
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-12-27 13:35:45 -0500

    [update]- mejoras en ui/ux en la galeria de foto de servicios - ventanas

--- src/data/gallery/ventana-data.js
+++ src/data/gallery/ventana-data.js
@@ -1,4 +1,3 @@
-import img_nova_a1 from "@/assets/services/products/01.Ventanas/nova/a1.jpeg";
 import img_nova_a2 from "@/assets/services/products/01.Ventanas/nova/a2.jpeg";
 import img_nova_a3 from "@/assets/services/products/01.Ventanas/nova/a3.jpeg";
 import img_nova_a4 from "@/assets/services/products/01.Ventanas/nova/a4.jpeg";
@@ -10,47 +9,53 @@ import img_nova_a9 from "@/assets/services/products/01.Ventanas/nova/a9.jpeg";
 import img_nova_a10 from "@/assets/services/products/01.Ventanas/nova/a10.jpeg";
 import img_nova_a11 from "@/assets/services/products/01.Ventanas/nova/a11.jpeg";
 import img_nova_a12 from "@/assets/services/products/01.Ventanas/nova/a12.jpeg";
+import img_nova_a13 from "@/assets/services/products/01.Ventanas/nova/a13.jpeg";
+import img_nova_a16 from "@/assets/services/products/01.Ventanas/nova/a16.jpeg";
 import img_serie_0294 from "@/assets/services/products/01.Ventanas/serie/IMG_0294.jpeg";
 import img_serie_0292 from "@/assets/services/products/01.Ventanas/serie/IMG_0292.jpeg";
 import img_serie_0799 from "@/assets/services/products/01.Ventanas/serie/IMG_0799.jpeg";
 import img_serie_0300 from "@/assets/services/products/01.Ventanas/serie/IMG_0300.jpeg";
 import img_serie_0304 from "@/assets/services/products/01.Ventanas/serie/IMG_0304.jpeg";
- import img_serie_0303 from "@/assets/services/products/01.Ventanas/serie/IMG_0303.jpeg";import img_serie_9209 from "@/assets/services/products/01.Ventanas/serie/IMG_9209.jpeg";
-import img_serie_110742 from "@/assets/services/products/01.Ventanas/serie/IMG_20211026_110742.jpeg";
-import img_serie_110803 from "@/assets/services/products/01.Ventanas/serie/IMG_20211026_110803.jpeg";
-import img_serie_114159 from "@/assets/services/products/01.Ventanas/serie/IMG_20211026_114159.jpeg";
+import img_serie_0303 from "@/assets/services/products/01.Ventanas/serie/IMG_0303.jpeg";
+import img_serie_9209 from "@/assets/services/products/01.Ventanas/serie/IMG_9209.jpeg";
+/* serie31 */
+import img_serie31_110742 from "@/assets/services/products/01.Ventanas/serie31/IMG_20211026_110742.jpeg";
+import img_serie31_110803 from "@/assets/services/products/01.Ventanas/serie31/IMG_20211026_110803.jpeg";
+import img_serie31_114159 from "@/assets/services/products/01.Ventanas/serie31/IMG_20211026_114159.jpeg";
 
 const _rawListVentana = {
     nova: [
-        { id: 1, image: img_nova_a1, name: "vn_01" },
+        { id: 10, image: img_nova_a10, name: "vn_10" },
+        { id: 7, image: img_nova_a7, name: "vn_07" },
+        { id: 1, image: img_nova_a13, name: "vn_13" },
         { id: 2, image: img_nova_a2, name: "vn_02" },
         { id: 3, image: img_nova_a3, name: "vn_03" },
         { id: 4, image: img_nova_a4, name: "vn_04" },
         { id: 5, image: img_nova_a5, name: "vn_05" },
         { id: 6, image: img_nova_a6, name: "vn_06" },
-        { id: 7, image: img_nova_a7, name: "vn_07" },
+
         { id: 8, image: img_nova_a8, name: "vn_08" },
         { id: 9, image: img_nova_a9, name: "vn_09" },
-        { id: 10, image: img_nova_a10, name: "vn_10" },
+
         { id: 11, image: img_nova_a11, name: "vn_11" },
         { id: 12, image: img_nova_a12, name: "vn_12" },
+        { id: 13, image: img_nova_a16, name: "vn_16" },
     ],
     serie25: [
-        { id: 1, image: img_serie_0294, name: "vs_01" },
-        { id: 2, image: img_serie_0292, name: "vs_02" },
-        { id: 3, image: img_serie_0799, name: "vs_03" },
-        { id: 4, image: img_serie_0294, name: "vs_08" },
-        { id: 5, image: img_serie_0300, name: "vs_04" },
-        { id: 6, image: img_serie_0304, name: "vs_05" },
-        { id: 7, image: img_serie_0303, name: "vs_06" },
-        { id: 8, image: img_serie_9209, name: "vs_09" },
+        { id: 1, image: img_serie_0294, name: "vs25_01" },
+        { id: 2, image: img_serie_0292, name: "vs25_02" },
+        { id: 3, image: img_serie_0799, name: "vs25_03" },
+        { id: 4, image: img_serie_0294, name: "vs25_04" },
+        { id: 5, image: img_serie_0300, name: "vs25_05" },
+        { id: 6, image: img_serie_0304, name: "vs25_06" },
+        { id: 7, image: img_serie_0303, name: "vs25_07" },
+        { id: 8, image: img_serie_9209, name: "vs25_08" },
     ],
     serie31: [
-        { id: 1, image: img_serie_0799, name: "vs_07" },
-        { id: 2, image: img_serie_110742, name: "vs_10" },
-        { id: 3, image: img_serie_110803, name: "vs_11" },
-        { id: 4, image: img_serie_114159, name: "vs_12" }
-    ]
+        { id: 1, image: img_serie31_110742, name: "vs31_01" },
+        { id: 2, image: img_serie31_110803, name: "vs31_02" },
+        { id: 3, image: img_serie31_114159, name: "vs31_03" },
+    ],
 };
 
 export const listVentana = _rawListVentana;
