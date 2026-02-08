8db065380fe82513a631ba2fb57354efe95473e7
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-02-08 14:46:10 -0500

    docs(jsdoc): Update useIsMobile.js with usage examples

--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -5,6 +5,21 @@ import { useMediaQuery } from "@chakra-ui/react";
  * Centraliza la lógica de `useMediaQuery` para una fácil reutilización y mantenimiento.
  *
  * @returns {boolean} Devuelve `true` si el ancho del viewport es de 768px o menos, `false` en caso contrario.
+ *
+ * @example
+ * // Ejemplo de uso en un componente React
+ * import { Text } from '@chakra-ui/react';
+ * import useIsMobile from '@/hooks/useIsMobile';
+ *
+ * function ResponsiveComponent() {
+ *   const isMobile = useIsMobile();
+ *
+ *   return (
+ *     <Text>
+ *       {isMobile ? "Estás en un dispositivo móvil" : "Estás en un dispositivo de escritorio"}
+ *     </Text>
+ *   );
+ * }
  */
 const useIsMobile = () => {
     // La media query "(max-width: 768px)" define el punto de quiebre para dispositivos móviles.
