0e7cf60cf88d5e95c85e971aa0ba6f4dc8d70159
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-02-08 23:05:45 -0500

    feat(home): add barrel exports for feature

---
+++ src/features/home/index.js
@@ -0,0 +1,20 @@
+/**
+ * @file index.js
+ * @description Feature Home - Barrel exports
+ */
+
+// Components
+export { default as ClientCard } from './components/ClientCard';
+export { default as ClientListSkeleton } from './components/ClientListSkeleton';
+export { default as ClientsSection } from './components/ClientsSection';
+export { default as FeatureCard } from './components/FeatureCard';
+export { default as FeatureListSkeleton } from './components/FeatureListSkeleton';
+export { default as FeaturesSection } from './components/FeaturesSection';
+export { default as LandingPageSection } from './components/LandingPageSection';
+export { default as StoreSection } from './components/StoreSection';
+
+// Hooks (none found)
+
+// Services
+export * from './services/clientService';
+export * from './services/featureService';
