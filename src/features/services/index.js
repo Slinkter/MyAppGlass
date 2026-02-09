3af3fb56a83a491d3873e6222d9816f35731faba
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-02-08 23:01:23 -0500

    feat(services): add barrel exports for feature

--- src/features/services/index.js
+++ src/features/services/index.js
@@ -5,8 +5,16 @@
 
 // Components
 export { default as ServiceCard } from './components/ServiceCard';
+export { default as ServiceCardContent } from './components/ServiceCardContent';
 export { default as ServiceList } from './components/ServiceList';
 export { default as ServiceListSkeleton } from './components/ServiceListSkeleton';
+export { default as ServicePageContainer } from './components/ServicePageContainer';
+export { default as ServicePageLayout } from './components/ServicePageLayout';
+export { default as ServiceSidebar } from './components/ServiceSidebar';
+export { default as ServiceSkeleton } from './components/ServiceSkeleton';
+export { default as SpecItem } from './components/SpecItem';
+
+// Hooks (none found)
 
 // Services
-export { getServices } from './services/serviceService';
+export * from './services/serviceService'; // Export all from serviceService.js
