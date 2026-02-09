e602f507f81195b20afa659eba04823178b8fdfb
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-02-08 22:27:43 -0500

    refactor(projects): update imports to use feature barrel

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import ProjectsList from "@/components/projects/ProjectsList";
+import { ProjectsList } from "@features/projects";
 
 /**
  * @component ProjectPage
