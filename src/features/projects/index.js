a7fc13360dad2f37ff8b642e06f1a0cec8e1178f
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-02-08 22:27:15 -0500

    feat(projects): add barrel exports for feature

---
+++ src/features/projects/index.js
@@ -0,0 +1,17 @@
+/**
+ * @file index.js
+ * @description Feature Projects - Barrel exports
+ */
+
+// Components
+export { default as ProjectCard } from './components/ProjectCard';
+export { default as ProjectsList } from './components/ProjectsList';
+export { default as ProjectDetailModal } from './components/ProjectDetailModal';
+export { default as ProjectCardSkeleton } from './components/ProjectCardSkeleton';
+export { default as ProjectListSkeleton } from './components/ProjectListSkeleton';
+
+// Hooks
+export { useProjectModal } from './hooks/useProjectModal';
+
+// Services
+export { getProjects } from './services/projectService';
