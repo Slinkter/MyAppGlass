13cce13616f0016d9df86fc9df4d5438bc3fd25d
Slinkter <luis.j.cueva@gmail.com>
2026-06-16 12:54:00 -0500

    Fase 4: Spacing responsive - BlogView py suavizado. Fase 5: Theme alignment verificado

--- src/screens/blog/ui/BlogView.tsx
+++ src/screens/blog/ui/BlogView.tsx
@@ -4,7 +4,7 @@ import BlogList from '@/features/blog/components/BlogList';
 
 export const BlogView: React.FC = () => {
   return (
-    <Box as='main' py={{ base: '14', md: '36' }}>
+    <Box as='main' py={{ base: '14', md: '24', lg: '36' }}>
       <BlogList />
     </Box>
   );
