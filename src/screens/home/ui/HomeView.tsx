ff3a29f419b8d1357e73dd3ff2353276fa74e16d
Slinkter <luis.j.cueva@gmail.com>
2026-07-24 18:46:55 -0500

    checkpoint: save baseline state before Vercel React best practices refactoring

--- src/screens/home/ui/HomeView.tsx
+++ src/screens/home/ui/HomeView.tsx
@@ -17,7 +17,7 @@ const HomeView: React.FC = React.memo(() => {
     return (
         <VStack gap="16" w="full" align="stretch">
             <LandingPageSection />
-            
+
             <Suspense fallback={<SectionSkeleton h="300px" />}>
                 <ClientsSection />
             </Suspense>
