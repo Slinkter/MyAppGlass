eefe517226aa94a34a797b43dbb2bf1536fa0f9a
Slinkter <luis.j.cueva@gmail.com>
2026-07-30 08:26:08 -0500

    fix(theme): update ColorModeProvider attribute to class to prevent Next.js script element warning

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -12,7 +12,7 @@ export type ColorModeProviderProps = ThemeProviderProps
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
   return (
-    <ThemeProvider attribute='data-theme' disableTransitionOnChange {...props} />
+    <ThemeProvider attribute='class' disableTransitionOnChange enableSystem={false} {...props} />
   )
 }
 
